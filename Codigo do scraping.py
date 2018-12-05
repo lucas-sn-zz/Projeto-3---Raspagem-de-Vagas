d928ab99242787e613bfc9582a0cf399d0c27f97
lucas-sn <lucas.silva.neris@gmail.com>
2018-12-05 19:25:39 -0200

    indeed

--- Codigo do scraping.py
+++ Codigo do scraping.py
@@ -1,62 +1,78 @@
 from selenium import webdriver
 import pandas as pd
 import time
-driver = webdriver.Firefox()
 from datetime import date
-nome_arquivo = str('dados'+'-'+ str(date.today().day) +'-' + str(date.today().month) + '-' + str(date.today().year) +'.csv')
-try:
-    tabela = pd.read_csv(nome_arquivo, sep = ',',index_col = 0)
-except:
-    tabela= pd.to_csv(nome_arquivo)
-def informacoes_pag():
-    #aqui serão armazenados os links encontrados a fim de entrar em cada página
-    #vamos fazer um for em cada link para pegar as informações
-    nome_empresas = 'BTG'
-    região = 'Rio de Janeiro'
-        driver.get('https://www.truckpad.com.br/fretes?page='+ str(y))
-        if y == 10 or 20 or 30 or 40 or 50 or 60 or 70 or 80 or 90:
-            tabela.to_csv('dados.csv', sep=',')
-        time.sleep(1)
-        print(y)
-        links = []
-        for z in driver.find_elements_by_xpath("//div[@class='container-freight media border-top position-relative']/a"):
+
+    
+def indeed_links():
+    try:
+        tabela = pd.read_csv(nome_arquivo, sep = ',',index_col = 0)
+    except:
+        tabela= pd.DataFrame()
+        tabela['vaga'] = pd.Series([])
+        tabela['empresa'] = pd.Series([])
+        tabela['cidade'] = pd.Series([])
+        tabela['link'] = pd.Series([])
+        tabela.to_csv(nome_arquivo)
+    links = []
+    #pegar os links da paginas
+    for z in driver.find_elements_by_xpath("//div[@class='jobsearch-SerpJobCard row result clickcard']/a"):
+        links.append(z.get_attribute('href'))
+    for z in driver.find_elements_by_xpath("//div[@class='jobsearch-SerpJobCard row sjlast result clickcard']/a"):
+        links.append(z.get_attribute('href'))
+    try:
+        for z in driver.find_elements_by_xpath("//div[@class='jobsearch-SerpJobCard row result clickcard']//h2[@class ='jobtitle']/a"):
             links.append(z.get_attribute('href'))
-        print(links)
-        for x in  range(len(links)):
-            print(x)
-            driver.get(links[x])
-        ##  print(links[x])
-            time.sleep(0.9)
-       ##     print('aqui foi')
-                ### print(x.get_attribute('href'))
-            infom = [links[x],cidade_origem(),cidade_destino(),data_coleta(),data_entrega(),distancia(),peso(),caminhoes(),(x +1)]
-            tabela.loc[-1] = infom
-            tabela.index = tabela.index + 1 
+    except:
+        pass
+
+    print(len(links))
+    for x in links:
+        time.sleep(2)
+        driver.get(x)
+        informacao = [indeed_titulo_vaga(),indeed_empresa_e_cidade()[0], indeed_empresa_e_cidade()[1],indeed_link_original()]
+        print(informacao)
+        tabela.loc[-1] = informacao
+        tabela.index = tabela.index + 1
+    tabela.to_csv("site_dados.csv")
+    tabela.to_excel("site_dados.xlsx")
+                
             
             
-def cidade_origem():
-    return(driver.find_elements_by_xpath("//div[@class='origin_destiny config_line_infos row col-12 col-sm-6 col-lg-6 margin-0px']//div[@class='col-7 p-0']/p[@class='description']")[0].text)
-
+            
+            
+        
     
-def cidade_destino():
-    return(driver.find_elements_by_xpath("//div[@class='origin_destiny config_line_infos row col-12 col-sm-6 col-lg-6 margin-0px']//div[@class='col-5 reset-padding-mobile']/p[@class='description']")[0].text)
+def indeed_titulo_vaga():
+    return(driver.find_element_by_xpath("//h3[@class='icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title']").text)
 
+    #Link original
+def indeed_link_original():
+    try: 
+        return(driver.find_element_by_xpath("//span[@id='originalJobLinkContainer' and @class = 'icl-u-lg-inline icl-us-xs-hide' ]/a").get_attribute('href'))
+
+    except:
+        return(driver.current_url)
+            
     
-def data_coleta():
-    return(driver.find_elements_by_xpath("//div[@class='principal_information config_line_infos row col-12 col-sm-6 col-lg-6 margin-0px']//div[@class='col-7 p-0']/p[@class='description']")[1].text)
 
-def data_entrega():
-    return(driver.find_elements_by_xpath("//div[@class='principal_information config_line_infos row col-12 col-sm-6 col-lg-6 margin-0px']//div[@class='col-5 reset-padding-mobile']/p[@class='description']")[1].text)
+def indeed_empresa_e_cidade():
+    # Empresa; Cidade; Estado
+    return(driver.find_element_by_xpath("//div[@class='jobsearch-InlineCompanyRating icl-u-xs-mt--xs  jobsearch-DesktopStickyContainer-companyrating']").text.split("\n-\n"))
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\Lucas Neris\Documents\GitHub\Projeto-3---Raspagem-de-Vagas\chromedriver.exe")
+nome_arquivo = str('dados'+'-'+ str(date.today().day) +'-' + str(date.today().month) + '-' + str(date.today().year) +'.csv')
+
+inputs = pd.read_excel("inputGERAL.xlsx",sheetname = "SHEET1")
+inputs.columns
+
+for x in range(len(inputs)):
+    assuntos = inputs.ix[x].Assunto
+    assuntos = assuntos.replace(" ","+")
+    cidades = inputs.ix[x].Cidade
+    cidades = cidades.replace(" ","+")
+    driver.get("https://www.indeed.com.br/empregos?q=" + assuntos + "&l=" + cidades)
     
-def distancia():
-    return(driver.find_elements_by_xpath("//div[@class='origin_destiny config_line_infos row col-12 col-sm-6 col-lg-6 margin-0px']//div[@class='col-5 reset-padding-mobile']/p[@class='description']")[1].text)
-      
-def peso():
-    return(driver.find_elements_by_xpath("//div[@class='principal_information config_line_infos row col-12 col-sm-6 col-lg-6 margin-0px']//div[@class='col-5 reset-padding-mobile']/p[@class='description']")[0].text)
+    
+indeed_links()
 
-
-def  caminhoes():
-    caminhoes = []
-    for x in driver.find_elements_by_xpath("//div[@class='vehicle_infos config_line_infos col-12']//p[@class='description float-left']/span"):
-        caminhoes.append(x.text)
-    return(caminhoes)
